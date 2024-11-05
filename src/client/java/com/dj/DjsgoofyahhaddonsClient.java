f9829f54f754fae069464cb70911cc9e9da65b0e
TheDJGamerYT <156543381+TheDJGamerYT@users.noreply.github.com>
2024-11-05 01:48:32 -0600

    e

---
+++ src/client/java/com/dj/DjsgoofyahhaddonsClient.java
@@ -0,0 +1,10 @@
+package com.dj;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DjsgoofyahhaddonsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
